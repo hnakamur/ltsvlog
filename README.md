ed0c529528dc7478e527f7626d43287d0be09570
Hiroaki Nakamura <hnakamur@gmail.com>
2016-05-22 19:07:50 +0000

    Add document

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 ltsvlog
 =======
 
-ltsvlog is a simple [LTSV; Labeled Tab-separated Values](http://ltsv.org/) logging library in Go.
+ltsvlog is a minimalist [LTSV; Labeled Tab-separated Values](http://ltsv.org/) logging library in Go.
+See https://godoc.org/github.com/hnakamur/ltsvlog for the API document.
 
 ## License
 MIT
--- log.go
+++ log.go
@@ -1,3 +1,16 @@
+// Package ltsvlog is a minimalist logging library for writing logs in
+// LTSV (Labeled Tab-separated Value) format.
+// See http://ltsv.org/ for LTSV.
+// This library is designed with emphasis on more performance than flexibility.
+//
+// The first value is the current time with the label "time".
+// The time format is RFC3339Nano UTC like 2006-01-02T15:04:05.999999999Z.
+// The second value is the log level with the label "level".
+// Then labeled values passed to Debug, Info, Error follows.
+//
+// Package ltsv provides three log levels: Debug, Info and Error.
+// The Info and Error levels are always enabled.
+// You can disable the Debug level but only when you create a logger.
 package ltsvlog
 
 import (
@@ -6,18 +19,21 @@ import (
 	"time"
 )
 
+// LV represents a label L and a value V.
 type LV struct {
 	L string
 	V string
 }
 
+// LTSVLogger is a LTSV logger.
 type LTSVLogger struct {
 	writer       io.Writer
-	buf          []byte
 	debugEnabled bool
+	buf          []byte
 	mu           sync.Mutex
 }
 
+// NewLTSVLogger creates a LTSV logger.
 func NewLTSVLogger(w io.Writer, debugEnabled bool) *LTSVLogger {
 	return &LTSVLogger{
 		writer:       w,
@@ -25,29 +41,36 @@ func NewLTSVLogger(w io.Writer, debugEnabled bool) *LTSVLogger {
 	}
 }
 
+// DebugEnabled returns whether or not the debug level is enabled.
+// You can avoid the cost of evaluation of arguments passed to Debug like:
+// if logger.DebugEnabled() { logger.Debug(ltsvlog.LTV{"label1": "value1"}) }
 func (l *LTSVLogger) DebugEnabled() bool {
 	return l.debugEnabled
 }
 
+// Debug writes a log with the debug level if the debug level is enabled.
 func (l *LTSVLogger) Debug(lv ...LV) {
 	if l.debugEnabled {
 		l.log("Debug", lv...)
 	}
 }
 
+// Info writes a log with the info level.
 func (l *LTSVLogger) Info(lv ...LV) {
 	l.log("Info", lv...)
 }
 
+// Error writes a log with the error level.
 func (l *LTSVLogger) Error(lv ...LV) {
 	l.log("Error", lv...)
 }
 
 func (l *LTSVLogger) log(level string, lv ...LV) {
-	now := time.Now().UTC()
 	l.mu.Lock()
-
+	// Note: To reuse the buffer, create an empty slice pointing to
+	// the previously allocated buffer.
 	buf := append(l.buf[:0], []byte("time:")...)
+	now := time.Now().UTC()
 	buf = appendZeroPaddedInt(buf, now.Year(), 1000)
 	buf = append(buf, byte('-'))
 	buf = appendZeroPaddedInt(buf, int(now.Month()), 10)
@@ -77,13 +100,13 @@ func (l *LTSVLogger) log(level string, lv ...LV) {
 	l.mu.Unlock()
 }
 
-func appendZeroPaddedInt(buf []byte, value, p int) []byte {
-	for ; p > 1; p /= 10 {
-		q := value / p
-		buf = append(buf, digits[q])
-		value -= q * p
-	}
-	return append(buf, digits[value])
-}
-
 var digits = []byte{'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'}
+
+func appendZeroPaddedInt(buf []byte, v, p int) []byte {
+	for ; p > 1; p /= 10 {
+		q := v / p
+		buf = append(buf, digits[q])
+		v -= q * p
+	}
+	return append(buf, digits[v])
+}
