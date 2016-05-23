869af78fe4b002b765fadeef049d4fadabac84f3
Hiroaki Nakamura <hnakamur@gmail.com>
2016-05-23 00:35:35 +0000

    Add benchmark

--- README.md
+++ README.md
@@ -4,5 +4,15 @@ ltsvlog
 ltsvlog is a minimalist [LTSV; Labeled Tab-separated Values](http://ltsv.org/) logging library in Go.
 See https://godoc.org/github.com/hnakamur/ltsvlog for the API document.
 
+## Benchmark result
+
+```
+# go test -bench . -benchmem -cpuprofile -memprofile
+...(snip)...
+BenchmarkLTSVLog-2       2000000               962 ns/op             197 B/op          0 allocs/op
+BenchmarkStandardLog-2   1000000              1194 ns/op             235 B/op          3 allocs/op
+...(snip)...
+```
+
 ## License
 MIT
---
+++ log_test.go
@@ -0,0 +1,23 @@
+package ltsvlog
+
+import (
+	"bytes"
+	"log"
+	"testing"
+)
+
+func BenchmarkLTSVLog(b *testing.B) {
+	w := new(bytes.Buffer)
+	logger := NewLTSVLogger(w, false)
+	for i := 0; i < b.N; i++ {
+		logger.Info(LV{"msg", "sample log message"}, LV{"key1", "value1"}, LV{"key2", "value2"})
+	}
+}
+
+func BenchmarkStandardLog(b *testing.B) {
+	w := new(bytes.Buffer)
+	logger := log.New(w, "", log.LstdFlags|log.Lmicroseconds)
+	for i := 0; i < b.N; i++ {
+		logger.Printf("msg:sample log message\tkey1:%s\tkey2:%s", "value1", "value2")
+	}
+}
